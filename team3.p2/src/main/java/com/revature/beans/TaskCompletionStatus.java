5b45eec91c2343b2d8f17258a30934511aa427fd
siddmohanty111 <70185258+siddmohanty111@users.noreply.github.com>
2021-08-17 12:19:10 -0700

    Delete team3.p2 directory

--- team3.p2/src/main/java/com/revature/beans/TaskCompletionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.revature.beans;
-
-public enum TaskCompletionStatus {
-
-	TODO, IN_PROGRESS, COMPLETED, BACKLOG
-	
-}
--- team3.p2/src/main/java/com/revature/data/UserDAO.java
+++
@@ -1,5 +0,0 @@
-package com.revature.data;
-
-public enum UserDAO {
-
-}
