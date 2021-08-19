a97480b9d3035168a349f12063db81430b0eca05
Sidd <siddhant_99@hotmail.com>
2021-08-19 12:32:00 -0700

    removing some fields from user and adding task enim

--- src/main/java/com/revature/data/UserDAO.java
+++ src/main/java/com/revature/data/UserDAO.java
@@ -1,5 +1,9 @@
 package com.revature.data;
 
-public enum UserDAO {
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
-}
+import com.revature.dto.UserDTO;
+
+@Repository
+public interface UserDAO extends CassandraRepository<UserDTO, String>{}
