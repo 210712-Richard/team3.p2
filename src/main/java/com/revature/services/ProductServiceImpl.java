414cb529b5c7f793f4654635be5a933f8a96bb6e
jmccall10 <jasminemccall880@yahoo.com>
2021-08-19 08:14:24 -0500

    testing for adding new product

--- src/main/java/com/revature/services/ProductServiceImpl.java
+++ src/main/java/com/revature/services/ProductServiceImpl.java
@@ -4,15 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.beans.Product;
+import com.revature.data.ProductDAO;
 import com.revature.data.ProductDao;
 import com.revature.dto.ProductDTO;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-	private ProductDao productDao;
+	private ProductDAO productDao;
 	
 	@Autowired
-	public ProductServiceImpl(ProductDao productDao) {
+	public ProductServiceImpl(ProductDAO productDao) {
 		super();
 		this.productDao = productDao;
 		
