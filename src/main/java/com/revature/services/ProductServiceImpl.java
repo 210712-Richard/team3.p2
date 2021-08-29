2342124e52c73da4b8ba28322272b8908f001075
Sidd <siddhant_99@hotmail.com>
2021-08-28 23:36:02 -0700

    fixing smells

--- src/main/java/com/revature/services/ProductServiceImpl.java
+++ src/main/java/com/revature/services/ProductServiceImpl.java
@@ -3,7 +3,6 @@ package com.revature.services;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
